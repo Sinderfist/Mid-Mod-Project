d61df75371c924395504e611894e1e8ddeb4a7ac
Sinderfist <donte_360@yahoo.com>
2021-05-08 16:42:47 -0400

    first commit

---
+++ src/Components/Display.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ShoppingListInput from './ShoppingListInput';
+
+class Display extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Make your build</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default Display;
+
